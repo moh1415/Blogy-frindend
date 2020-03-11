6ebd155908b1e6c6f2f974ae0f235744a212438e
moh1415 <moh14151415@gmail.com>
2020-03-11 14:52:24 +0300

    Add config for connecting with the API server based on the environment.

---
+++ src/articles/components/articles.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+ export default class Articles extends React.Component {
+
+render() {
+    return (
+        <div>
+            <h3>All Articles</h3>
+        </div>
+    );
+}
+}
